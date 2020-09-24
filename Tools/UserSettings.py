9cbdd357fa0f0b6202e6da30825d69b7a83f1b5d
noifuji <ryoma1025@gmail.com>
2020-09-24 16:58:04 +0900

    a

---
+++ Tools/UserSettings.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from sikuli import *
+
+#slack
+TOKEN = ""
+TARGET_CHANNEL = ""
+
+#Google Drive
+DRIVE_DECK_CODE_JSON_URL = ""
+ACCOUNT_INFO_SHEET_ID = ""
+
+mentionUser = '' # Ryoma : U017DKDFGER, Bolmeteus : U017DKN47A9
+AppPath = r""
+AndAppPath = r""
+NoxDirPath = r""
+RUN_MODE = ""
+
+
+NOX_INSTANCES = [] #[Image, ref]
