8970ca33f2fdcec15d7d0d96d6d5142d4026cca3
Ryoma <ryoma1025@gmail.com>
2020-10-02 14:27:46 +0900

    auto update modified

--- Tools/auto_update.bat
+++ Tools/auto_update.bat
@@ -1,5 +1,5 @@
 cd %~dp0\..
 git fetch https://github.com/noifuji/dmp-auto.git
 git reset --hard FETCH_HEAD
-start .\Tools\DMPAutoDailyProcesses.bat
+start .\Tools\%1
 exit
--- bat/Nox_DailyMission.bat
+++ bat/Nox_DailyMission.bat
@@ -7,5 +7,9 @@ set PIDFile=./data/pid.txt
 powershell "Get-WmiObject win32_process -filter processid=$pid | ForEach-Object{$_.parentprocessid;}" > %PIDFile%
 
 java -jar sikulixide-2.0.4.jar -r ./scripts/NoxDailyMission.sikuli
+if %ERRORLEVEL%==50 (
+ start .\Tools\auto_update.bat %~n0%~x0
+ exit
+)
 
 pause
